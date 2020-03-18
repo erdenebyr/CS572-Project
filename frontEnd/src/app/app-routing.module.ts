744f92f07a1553a449a1ab300475fdc2b36d3c02
Edy <erdenebyr@gmail.com>
2020-03-18 12:09:20 -0500

    clear profile section and edit section

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 import { AuthGuard } from '../app/_helpers/auth.guard'
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
-  { path: 'editprofile', component: EditProfileComponent, canActivate: [AuthGuard]}, 
+  // { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  // { path: 'profileEdit', component: ProfileEditComponent, canActivate: [AuthGuard]}, 
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
 
---
+++ frontEnd/src/app/home-timeline/home-timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-timeline',
+  templateUrl: './home-timeline.component.html',
+  styleUrls: ['./home-timeline.component.css']
+})
+export class HomeTimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
